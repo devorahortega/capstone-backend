798b318397e8dd57400c57aa6738a4c9e29d8e1f
Devorah Ortega <devorahb.ortega@gmail.com>
2021-11-19 14:49:32 -0500

    Creates the CRUD actions for prompts

---
+++ app/controllers/prompts_controller.rb
@@ -0,0 +1,45 @@
+class PromptsController < ApplicationController
+  def index
+    prompts = Prompt.all
+    render json: prompts
+  end
+
+  def show
+    prompt = Prompt.find_by(id: params[:id])
+    render json: prompt
+  end
+
+  def create
+    prompt = Prompt.new(
+      title: params["title"],
+      content: params["content"],
+    )
+    if prompt.save
+      render json: prompt
+    else
+      render json: { errors: prompt.error.full_messages }, status: 422
+    end
+  end
+
+  def update
+    prompt_id = params["id"]
+    prompt = Prompt.find_by(id: prompt_id)
+
+    prompt.title = params["title"] || prompt.title
+    prompt.content = params["content"] || prompt.content
+
+    if prompt.save
+      render json: prompt
+    else
+      render json: { errors: prompt.error.full_messages }, status: 422
+    end
+  end
+
+  def destroy
+    prompt_id = params["id"]
+    prompt = Prompt.find_by(id: prompt_id)
+
+    prompt.destroy
+    render json: { message: "This prompt has been destroyed" }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/prompts" => "prompts#index"
+  get "/prompts/:id" => "prompts#show"
+  post "/prompts" => "prompts#create"
+  patch "/prompts/:id" => "prompts#update"
+  delete "/prompts/:id" => "prompts#destroy"
 end
