fe3d48445a568a56abeb747ee99841a87de084d3
Devorah Ortega <devorahb.ortega@gmail.com>
2021-12-16 13:30:08 -0500

    fixes prompt serializer issue by commenting out prompt tag association.

--- app/serializers/prompt_serializer.rb
+++ app/serializers/prompt_serializer.rb
@@ -1,7 +1,7 @@
 class PromptSerializer < ActiveModel::Serializer
   attributes :id, :title, :content
 
-  has_many :prompttags
+  # has_many :prompttags
   has_many :favorites
   has_many :users, through: :favorites
 end
