20b5e9070b6d52e08de8c8a8be951711552aa7b9
Devorah Ortega <devorahb.ortega@gmail.com>
2022-10-31 11:25:14 -0500

    Changes passwords in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@ Prompt.create!({ title: "Dungeon Dragon", content: "Traveling through an undergr
 Prompt.create!({ title: "Murder on the Northern Express", content: "A murder has occured on the northern express, the only clues are a hairpin and a man's left shoe." })
 Prompt.create!({ title: "The Taker", content: "Life in a world that slowly but surely takes away all the color and sensation of the people for the purpose of making them happier." })
 
-User.create!({ first_name: "Devorah", last_name: "Ortega", username: "devort", password: "password", password_confirmation: "password" })
-User.create!({ first_name: "Bob", last_name: "Green", username: "bobgre", password: "password", password_confirmation: "password" })
-User.create!({ first_name: "Jane", last_name: "Doe", username: "jandoe", password: "password", password_confirmation: "password" })
+User.create!({ first_name: "Devorah", last_name: "Ortega", username: "devort", password: "1password1", password_confirmation: "1password1" })
+User.create!({ first_name: "Bob", last_name: "Green", username: "bobgre", password: "2password2", password_confirmation: "2password2" })
+User.create!({ first_name: "Jane", last_name: "Doe", username: "jandoe", password: "3password3", password_confirmation: "3password3" })
 
 Tag.create!({ tag: "fantasy" })
 Tag.create!({ tag: "mystery" })
